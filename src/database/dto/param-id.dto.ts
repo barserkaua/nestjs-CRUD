fe2cb24b71e583fee07561690b49f6575202478d
barserkaua <barserkaua@gmail.com>
2024-04-07 21:44:50 +0300

    fix: :bug: added ApiProperty to ParamID

--- src/database/dto/param-id.dto.ts
+++ src/database/dto/param-id.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class ParamID {
+  @ApiProperty({ example: '94423239-3122-46fb-8f54-ed86aa2ee0f6' })
   @IsNumber()
   id: string;
 }
